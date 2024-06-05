3afbf7acaa200b128d19bd8ac2c77e5ae1910702
jslim <junseojunseo@naver.com>
2024-06-06 01:01:08 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
+
+import uvicorn
+
+app = FastAPI()
+
+
+
+
+
+# CORS 설정 추가
+origins = [
+    "http://127.0.0.1:5500",
+    "aws 주소 !!!!",
+    
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class GuestbookEntry(BaseModel):
+    id: int
+    writer: str
+    content: str
+    timestamp: datetime
+
+class GuestbookEntryCreate(BaseModel):
+    writer: str
+    content: str
+
+guestbook_entries = []
+
+@app.get("/guestbook", response_model=List[GuestbookEntry])
+def read_guestbook():
+    return guestbook_entries
+
+@app.post("/guestbook", response_model=GuestbookEntry)
+def create_entry(entry: GuestbookEntryCreate):
+    new_entry = GuestbookEntry(
+        id=len(guestbook_entries) + 1,  
+        writer=entry.writer,
+        content=entry.content,
+        timestamp=datetime.now()
+    )
+    guestbook_entries.append(new_entry)
+    return new_entry
+
+@app.delete("/guestbook/{entry_id}")
+def delete_entry(entry_id: int):
+    for entry in guestbook_entries:
+        if entry.id == entry_id:
+            guestbook_entries.remove(entry)
+            return {"message": "Entry deleted successfully"}
+    return {"message": "Entry not found"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
